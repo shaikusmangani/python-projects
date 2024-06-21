fbc2cf7972c52554f8fb82923aa625724541ca44
shaikusmangani <usmanganiayesha@gmail.com>
2024-06-21 10:47:25 +0530

    Create demonstration of set operations.py

---
+++ demonstration of set operations.py
@@ -0,0 +1,12 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+union_result = E.union(N)
+intersection_result = E.intersection(N)
+difference_result = E.difference(N)
+symmetric_difference_result = E.symmetric_difference(N)
+
+print(f"Union of E and N is {union_result}")
+print(f"Intersection of E and N is {intersection_result}")
+print(f"Difference of E and N is {difference_result}")
+print(f"Symmetric difference of E and N is {symmetric_difference_result}")
